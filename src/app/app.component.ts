d81245a032c8c2e3b007e868b199114b0f20ad07
Oliver Kuffner <olk@dataone.de>
2018-08-03 15:47:16 +0200

    added chartjs radar + echarts framework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, AfterViewInit, OnInit } from '@angular/core';
 import * as Chart from 'chart.js'
 import { HttpService } from './http.service';
-import { Observable, BehaviorSubject } from '../../node_modules/rxjs';
+import { BehaviorSubject } from '../../node_modules/rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +9,11 @@ import { Observable, BehaviorSubject } from '../../node_modules/rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements AfterViewInit, OnInit {
-  private pieValues = new BehaviorSubject(null);
+  private iceValues = new BehaviorSubject(null);
+  private developerValues = new BehaviorSubject(null);
   private pieChart: Chart;
+
+  private echarts = window['echarts'];
 
   constructor(private httpService: HttpService) { }
 
@@ -19,76 +22,174 @@ export class AppComponent implements AfterViewInit, OnInit {
     Chart.defaults.global.defaultFontFamily = "'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif";
     Chart.defaults.global.defaultFontSize = 13;
 
-    this.httpService.getPieValues()
-      .subscribe(value => this.pieValues.next(value));
+    this.httpService.getIceCreamValues()
+      .subscribe(value => this.iceValues.next(value));
+
+    this.httpService.getDeveloperValues()
+      .subscribe(value => this.developerValues.next(value));
   }
 
   ngAfterViewInit() {
-    this.pieValues.subscribe(value => {
-      if (!value) {
-        return;
-      }
-      const labels = (<any[]>value.results).map(({ label }) => label);
-      const colors = (<any[]>value.results).map(({ color }) => color);
-      const amounts = (<any[]>value.results).map(({ value }) => value);
-
-      const pieCtx = this.getCanvasContext('pie-canvas');
-
-      this.pieChart = new Chart(pieCtx, {
-        type: 'pie', // doughnut
-        data: {
-          labels: labels,
-          datasets: [{
-            data: amounts,
-            backgroundColor: colors,
-            borderWidth: 1
-          }]
-        },
-        options: {
-          title: { text: value.name, display: true }
-        }
-      });
+    this.iceValues.subscribe(value => {
+      this.initCharJsIceCharts(value);
+      this.initEChartsIceCharts(value);
     });
 
-    // const barCtx = this.getCanvasContext('bar-canvas');
-    // var barChart = new Chart(barCtx, {
-    //   type: 'bar',
-    //   data: {
-    //     labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    //     datasets: [{
-    //       label: '# of Votes',
-    //       data: [12, 19, 3, 5, 2, 3],
-    //       backgroundColor: [
-    //         'rgba(255, 99, 132, 0.2)',
-    //         'rgba(54, 162, 235, 0.2)',
-    //         'rgba(255, 206, 86, 0.2)',
-    //         'rgba(75, 192, 192, 0.2)',
-    //         'rgba(153, 102, 255, 0.2)',
-    //         'rgba(255, 159, 64, 0.2)'
-    //       ],
-    //       borderColor: [
-    //         'rgba(255,99,132,1)',
-    //         'rgba(54, 162, 235, 1)',
-    //         'rgba(255, 206, 86, 1)',
-    //         'rgba(75, 192, 192, 1)',
-    //         'rgba(153, 102, 255, 1)',
-    //         'rgba(255, 159, 64, 1)'
-    //       ],
-    //       borderWidth: 1
-    //     }]
-    //   },
-    //   options: {
-    //     scales: {
-    //       yAxes: [{
-    //         ticks: {
-    //           beginAtZero: true,
-    //           display: true
-    //         }
-    //       }]
-    //     }
-    //   }
-    // });
+    this.developerValues.subscribe(value => {
+      this.initChartJsDevCharts(value);
+    });
+  }
 
+  private initCharJsIceCharts(value) {
+    if (!value) {
+      return;
+    }
+    const labels = (<any[]>value.results).map(({ label }) => label);
+    const colors = (<any[]>value.results).map(({ color }) => color);
+    const amounts = (<any[]>value.results).map(({ value }) => value);
+
+    const pieCtx = this.getCanvasContext('pie-canvas');
+
+    this.pieChart = new Chart(pieCtx, {
+      type: 'pie', // doughnut
+      data: {
+        labels: labels,
+        datasets: [{
+          data: amounts,
+          backgroundColor: this.getTransparentColors(colors),
+          hoverBackgroundColor: colors,
+          borderWidth: 1
+        }]
+      },
+      options: {
+        title: { text: value.name, display: true },
+        animation: {
+          easing: 'easeOutBounce',
+          duration: 1200
+        }
+      }
+    });
+
+    const previousAmounts = amounts.map(a => { return Math.abs(a + this.getRandomNumber(-5, 5)) });
+
+    const barCtx = this.getCanvasContext('bar-canvas');
+    new Chart(barCtx, {
+      type: 'bar',
+      data: {
+        labels: labels,
+        datasets: [{
+          label: '2017',
+          data: previousAmounts,
+          backgroundColor: this.getTransparentColors(colors),
+          hoverBackgroundColor: colors,
+          borderWidth: 1
+        },
+        {
+          label: '2018',
+          data: amounts,
+          backgroundColor: this.getTransparentColors(colors),
+          hoverBackgroundColor: colors,
+          borderWidth: 1,
+          borderColor: '#3a3a3a'
+        },
+        {
+          label: 'Durchschnitt',
+          data: this.createProcessData(previousAmounts, amounts),
+          type: 'line',
+          fill: false,
+          borderColor: '#aea2eb',
+          pointRadius: 4,
+          pointBackgroundColor: '#afcfeb'
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true,
+              display: true
+            }
+          }]
+        },
+        title: { text: value.name, display: true },
+        legend: { position: 'top' }
+      }
+    });
+  }
+
+  private initChartJsDevCharts(value: any) {
+    if (!value) {
+      return;
+    }
+    let datasets = [];
+    let i = 0;
+    value.values.forEach(data => {
+      datasets.push({
+        data: data,
+        backgroundColor: this.getTransparentColor(this.getRandomColor(), 0.5),
+        label: value.labels[i++]
+      });
+
+    });
+
+    const radarCtx = this.getCanvasContext('radar-canvas');
+    new Chart(radarCtx, {
+      type: 'radar',
+      data: {
+        labels: value.attributes,
+        datasets: datasets
+      }
+    });
+  }
+
+  private initEChartsIceCharts(value: any) {
+    if (!value) {
+      return;
+    }
+    const barChart = this.echarts.init(document.getElementById('echart-bar'));
+    const labels = (<any[]>value.results).map(({ label }) => label);
+    const amounts = (<any[]>value.results).map(({ value }) => value);
+    const colors = (<any[]>value.results).map(({ color }) => color);
+
+    const option = {
+      tooltip: {
+        show: true
+      },
+      legend: {
+        data: [value.name]
+      },
+      xAxis: [
+        {
+          type: 'category',
+          data: labels
+        }
+      ],
+      yAxis: [
+        {
+          type: 'value'
+        }
+      ],
+      series: [
+        {
+          "name": value.name,
+          "type": "bar",
+          "data": amounts
+        },
+        {
+          "name": value.name,
+          "type": "bar",
+          "data": amounts
+        }
+      ],
+      color: [ 
+        '#ff7f50', '#87cefa', '#da70d6', '#32cd32', '#6495ed', 
+        '#ff69b4', '#ba55d3', '#cd5c5c', '#ffa500', '#40e0d0', 
+        '#1e90ff', '#ff6347', '#7b68ee', '#00fa9a', '#ffd700', 
+        '#6b8e23', '#ff00ff', '#3cb371', '#b8860b', '#30e0e0' 
+    ]
+    };
+
+    barChart.setOption(option);
   }
 
   addToPieChart(name: string, amount: number) {
@@ -97,12 +198,25 @@ export class AppComponent implements AfterViewInit, OnInit {
     }
     this.pieChart.data.labels.push(name);
     (<number[]>this.pieChart.data.datasets[0].data).push(amount);
-    (<string[]>this.pieChart.data.datasets[0].backgroundColor).push(this.getRandomColor());
+    const color = this.getRandomColor();
+    (<string[]>this.pieChart.data.datasets[0].backgroundColor)
+      .push(this.getTransparentColor(color, 0.85));
+    (<string[]>this.pieChart.data.datasets[0].hoverBackgroundColor)
+      .push(color);
 
     this.pieChart.update();
   }
 
-  getCanvasContext(name: string) {
+  private createProcessData(values1: number[], values2: number[]): number[] {
+    const combined = [];
+    for (let i = 0; i < values1.length; i++) {
+      const average = (values1[i] + values2[i]) / 2;
+      combined.push(average);
+    }
+    return combined;
+  }
+
+  private getCanvasContext(name: string) {
     const canvas = <HTMLCanvasElement>document.getElementById(name);
     return canvas.getContext('2d');
   }
@@ -115,6 +229,21 @@ export class AppComponent implements AfterViewInit, OnInit {
   }
 
   private getRGBValue(): number {
-    return Math.floor(Math.random() * 256);
+    return this.getRandomNumber(0, 255);
+  }
+
+  private getRandomNumber(min: number, max: number): number {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  private getTransparentColors(colors: string[]): string[] {
+    return colors.map(c => {
+      return this.getTransparentColor(c, 0.85);
+    });
+  }
+
+  private getTransparentColor(color: string, alpha: number): string {
+    return color.replace('rgb', 'rgba')
+      .replace(')', ',' + alpha + ')');
   }
 }
--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -5,50 +5,104 @@ import { Observable } from '../../node_modules/rxjs';
   providedIn: 'root'
 })
 export class HttpService {
+  private iceValues = {
+    name: 'Lieblingseis',
+    results: [
+      {
+        label: 'Vanille',
+        value: this.getRandomNumber(5, 12),
+        color: 'rgb(255, 206, 86)'
+      },
+      {
+        label: 'Schokolade',
+        value: this.getRandomNumber(6, 12),
+        color: 'rgb(74, 48, 0)'
+      },
+      {
+        label: 'Erdbeer',
+        value: this.getRandomNumber(3, 8),
+        color: 'rgb(255, 99, 132)'
+      },
+      {
+        label: 'Zitrone',
+        value: this.getRandomNumber(2, 8),
+        color: 'rgb(255, 250, 59)'
+      },
+      {
+        label: 'Schlumpf',
+        value: this.getRandomNumber(1, 4),
+        color: 'rgb(54, 162, 235)'
+      }
+    ]
+  };
 
-  public getPieValues() {
-    const fakeValues = {
-      name: 'Lieblingseis',
-      results: [
-        {
-          label: 'Vanille',
-          value: this.getRandomNumber(5, 12),
-          color: 'rgb(255, 206, 86)'
-        },
-        {
-          label: 'Schokolade',
-          value: this.getRandomNumber(6, 12),
-          color: 'rgb(74, 48, 0)'
-        },
-        {
-          label: 'Erdbeer',
-          value: this.getRandomNumber(3, 8),
-          color: 'rgb(255, 99, 132)'
-        },
-        {
-          label: 'Zitrone',
-          value: this.getRandomNumber(2, 8),
-          color: 'rgb(255, 250, 59)'
-        },
-        {
-          label: 'Schlumpf',
-          value: this.getRandomNumber(1, 4),
-          color: 'rgb(54, 162, 235)'
-        }
-      ]
-    };
-    return this.getDeferredObservable(fakeValues);
+  private developerAttributes = ['Browser', 'Chat', 'Console', 'Email client', 'File explorer', 'IDE'];
+
+  private developerValues = {
+    attributes: this.developerAttributes,
+    values: [this.generateDeveloperValues(), this.shuffleArray(this.generateDeveloperValues())],
+    labels: ['Frontend Developer', 'Backend Developer']
+  };
+
+  public getIceCreamValues() {
+    return this.getDeferredObservable(this.iceValues);
   }
 
-  getDeferredObservable<T>(value: T): Observable<T> {
+  public getDeveloperValues() {
+    return this.getDeferredObservable(this.developerValues);
+  }
+
+  private getDeferredObservable<T>(value: T): Observable<T> {
     return Observable.create(observer => {
       setTimeout(() => {
         observer.next(value);
-      }, this.getRandomNumber(1000, 2500));
+      }, this.getRandomNumber(500, 1500));
     });
   }
 
   private getRandomNumber(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+
+  private generateDeveloperValues() {
+    const percentages = this.getRandomPercentages(6);
+    return [
+      percentages[0], percentages[1], percentages[2],
+      percentages[3], percentages[4], percentages[5],
+    ];
+  }
+
+  // TODO
+  private getRandomPercentages(amount: number): number[] {
+    const percentages = [];
+    let sum = 0;
+
+    for (let i = 0; i < amount; i++) {
+      let min = 5;
+      let max = 40;
+
+      if (i > 0) {
+        max = (100 - sum) / amount;
+      }
+
+      let p = Math.floor(this.getRandomNumber(min, max));
+
+      if (i == amount - 1) {
+        p = 100 - sum;
+      }
+
+      sum += p;
+      percentages[i] = p;
+    }
+    return percentages;
+  }
+
+  private shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
 }
